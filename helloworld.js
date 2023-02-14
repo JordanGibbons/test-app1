d94c084f3d9806af573b7ee276e34d30bcc9213c
JordanGibbons <49799084+JordanGibbons@users.noreply.github.com>
2023-02-14 12:46:51 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -4,4 +4,9 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Hello World');
 });
+try {
+  throw new Error("Testing an error message and stack...");
+} catch (error) {
+  console.error(error);
+}
 server.listen(8080);
